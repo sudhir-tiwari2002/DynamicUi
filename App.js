e1b0393e7097258fdf9fdf6cd5be16359a16f3b0
sudhir.pw <skt2ktiwari@gmail.com>
2023-12-06 16:34:04 +0530

    safe area view

--- App.js
+++ App.js
@@ -1,46 +1,43 @@
 
-import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import { StyleSheet,Text, View , SafeAreaView} from 'react-native';
+
 
 export default function App() {
- 
-const windowWidth=useWindowDimensions().width
-const windowHight = useWindowDimensions().height
- 
-
-  
   return (
+    <SafeAreaView style={styles.SafeContainer}>
     <View style={styles.container}>
-      <View style={[styles.box ,{width: windowWidth > 500 ? "70%" : "90%",height: windowHight > 600 ? "60%" : "90%",},]}>
-        <Text style={{fontSize: windowWidth >500 ? 50 : 24}}> welcome </Text>
+      <View style={styles.box}>
+        <Text style={styles.text}> welcome </Text>
       </View>
     </View>
+    </SafeAreaView>
   );
 }
 
-// const windowWidth = Dimensions.get("window").width;
-// const windowHight = Dimensions.get("window").height;
-
-// console.log(windowHight, windowWidth);
-
 const styles = StyleSheet.create({
+  SafeContainer :{
+    flex:1,
+    backgroundColor:"plum"
+  },
   container: {
     flex: 1,
-    backgroundColor: "plum",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: 'plum',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  box: {
+  box:{
+    padding:20
     // width:300,
     // height:300,
     // width:"70%",
     // height:"70%",
-    // width: windowWidth > 500 ? "70%" : "90%",
-    // height: windowHight > 600 ? "60%" : "90%",
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
+    // backgroundColor:"lightblue",
+    // alignItems:"center",
+    // justifyContent:"center"
   },
-  // text: {
-  //   fontSize: windowWidth > 500 ? 50 : 24,
-  // },
+  text:{
+    fontSize:24,
+    fontWeight : "bold",
+    textAlign: "center"
+  }
 });
