15b0e43dec1340c7e9a42eb24a171195cc5f700e
sudhir.pw <skt2ktiwari@gmail.com>
2023-12-06 15:47:22 +0530

    dynamic dymension api

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+
+import { StyleSheet,Text, View , Dimensions} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <View style={styles.box}>
+        <Text style={styles.text}> welcome </Text>
+      </View>
     </View>
   );
 }
+
+const windowWidth = Dimensions.get("window").width
+const windowHight = Dimensions.get("window").height
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +22,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box:{
+    // width:300,
+    // height:300,
+    // width:"70%",
+    // height:"70%",
+    width: windowWidth>500 ? "70%" : "90%",
+    height: windowHight> 600 ? "60%" : "90%",
+    backgroundColor:"lightblue",
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  text:{
+    fontSize:windowWidth>500 ? 50 : 24
+  }
 });
